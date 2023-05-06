0fb81567af6e7e39da219f1d4b500464c57db467
okzfans <zhangfan@cloudtrek.cn>
2023-05-06 18:30:12 +0800

    feat：新增异步更新，nextTick功能

--- src/index.js
+++ src/index.js
@@ -1,11 +1,13 @@
 // vue
 import { initMinix } from "./init"
 import { initLifeCycle } from "./lifecycle"
+import { nextTick } from "./observe/watcher"
 
 function Vue(options){ // options => 用户的选项
     this._init(options)
 }
 
+Vue.prototype.$nextTick = nextTick
 initMinix(Vue)
 initLifeCycle(Vue)
 
